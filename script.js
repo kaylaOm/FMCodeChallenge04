9e036df8ce00cb844dd065d479bb5b52469e4a96
KaylaOM <91630567+kaylaOm@users.noreply.github.com>
2021-12-06 14:23:17 -0600

    Responsive, Rough JS

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var answer = this.previousElementSibling.children[1];
+    var question = this.previousElementSibling.children[0];
     if (answer.style.display === "block") {
       answer.style.display = "none";
+      question.style.font = "12px Kumbh Sans, sans-serif";
 
     } else {
       answer.style.display = "block";
+      question.style.font = "bold 12px Kumbh Sans, sans-serif";
     }
   });
 }
